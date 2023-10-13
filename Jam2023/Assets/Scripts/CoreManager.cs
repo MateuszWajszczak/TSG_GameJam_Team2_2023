e27c3d6777bcb379bd41d6140384cc91c6bb10ba
MateuszWajszczak <matwaj2693@gmail.com>
2023-10-13 14:16:16 +0200

    Try using dictionaries for Keys and Collectibles

--- Jam2023/Assets/Scripts/CoreManager.cs
+++ Jam2023/Assets/Scripts/CoreManager.cs
@@ -16,7 +16,11 @@ public class CoreManager : MonoBehaviour
 
     //UI Elements
     [SerializeField] private TextMeshProUGUI timertextMeshProUGUI;
-    [SerializeField] private TextMeshProUGUI objecttextMeshProUGUI; 
+    [SerializeField] private TextMeshProUGUI objecttextMeshProUGUI;
+
+    //Keys and Collectibles
+    private Dictionary<int, bool> keyStatus = new Dictionary<int, bool>();
+    private Dictionary<int, bool> collectibleStatus = new Dictionary<int, bool>();
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,18 @@ public class CoreManager : MonoBehaviour
         playerToggle = FindObjectOfType<FlashlightSystem>();
         UpdatePickUpObjectText();
         darkBatteryCurrent = darkBatteryStart;
+
+        //Keys
+        keyStatus.Add(1, false); 
+        keyStatus.Add(2, false);
+        keyStatus.Add(3, false);
+        keyStatus.Add(4, false);
+
+        //Collectibles
+        collectibleStatus.Add(1, false);
+        collectibleStatus.Add(2, false);
+        collectibleStatus.Add(3, false);
+        collectibleStatus.Add(4, false);
     }
 
     // Update is called once per frame
@@ -64,5 +80,21 @@ public class CoreManager : MonoBehaviour
     {
         int percentage = Mathf.RoundToInt((darkBatteryCurrent / darkBatteryStart) * 100);
         timertextMeshProUGUI.text = percentage.ToString() + "%";
+    }
+
+    public void CollectKey(int keyID)
+    {
+        if (keyStatus.ContainsKey(keyID))
+        {
+            keyStatus[keyID] = true; // Set the status to collected
+        }
+    }
+
+    public void CollectColectible(int collectibleID)
+    {
+        if (collectibleStatus.ContainsKey(collectibleID))
+        {
+            collectibleStatus[collectibleID] = true; // Set the status to collected
+        }
     }
 }
